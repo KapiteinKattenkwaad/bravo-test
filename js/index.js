0b7150b7364e8e7cc9ecfad54ffa08330b2607b5
Max Stouten <maxstouten@gmail.com>
2021-12-18 11:38:45 +1100

    mobile events

--- js/index.js
+++ js/index.js
@@ -16,9 +16,11 @@ function appendData(data) {
     for (let i = 0; i < data.length; i++) {
         const event = document.createElement("article");
         event.className = "events__detail"
-        event.innerHTML = `<img src="${data[i].imgUrlDesktop}"> 
+        event.innerHTML = `<img class="events__detail--img" srcset="${data[i].imgUrlMobile} 768w, ${data[i].imgUrlDesktop} 1200w"  sizes="(max-width: 768px) 768px,
+            1200px" src="${data[i].imgUrlDesktop}"> 
                             <div class="events__detail--inner"> 
                                 <h3>${data[i].title}</h3> <p>${data[i].description}</p> 
+                                <img class="events__detail--btn" src="./assets/right_chevron.svg" alt="button right">
                              </div>`
         mainContainer.appendChild(event);
     }
